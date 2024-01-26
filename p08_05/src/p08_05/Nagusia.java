65162f200712b2ca0d599e2ac1190916a5841edc
iorilope <146725975+iorilope@users.noreply.github.com>
2024-01-26 12:41:52 +0100

    Ariketa Bukatuta

--- p08_05/src/p08_05/Nagusia.java
+++ p08_05/src/p08_05/Nagusia.java
@@ -1,5 +1,6 @@
-/**
- * 
+/*
+ * 2024(e)ko urt. 26(a)
+ * Ioritz Lopetegi
  */
 package p08_05;
 
@@ -7,7 +8,7 @@ package p08_05;
 
 import java.awt.FlowLayout;
 import java.lang.Math;
-import java.awt.GridLayout;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -16,42 +17,68 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.WindowConstants;
 
+
 /**
  * 
  * @author Ioritz Lopetegi
  */
 public class Nagusia {
 
+	/**  Frame nagusia. */
 	private static JFrame frame = new JFrame("Dadoak");
+
+	/**  Dadoen frame-a. */
 	private static JFrame frame2 = new JFrame("Partida");
+
+	/**  Erantzunen frame-a. */
 	private static JFrame frame3 = new JFrame("Emaitza");
 
-
+	/**  Label Nagusia. */
 	private static  JLabel label1 = new JLabel("Dadoak Aukeratu");
+
+	/**  Label Aukeratu. */
 	private static  JLabel label2 = new JLabel("Aukeratu dado kopurua");
+
+	/**  Botoia aukeratu dadoa. */
 	private static JButton botoia1 = new JButton("Aukeratu dadoak");
+
+	/** BotoidaDado4. */
 	private static JButton botoiadado4 = new JButton("4eko dadoak");
+
+	/** BotoiaDado6. */
 	private static JButton botoiadado6 = new JButton("6eko dadoak");
+
+	/** BotoiaDado10. */
 	private static JButton botoiadado10 = new JButton("10eko dadoak");
+
+	/** BotoiaDado20. */
 	private static JButton botoiadado20 = new JButton("20eko dadoak");
+
+	/** Textfield Dadokopurua. */
 	private static JTextField textfield1 =new JTextField();
+
+	/** The lbltext 2. */
 	private static  JLabel lbltext2 = new JLabel();
+
+	/** The lbltext 3. */
 	private static  JLabel lbltext3 = new JLabel();
 
-
+	/** The TxtFldKopuruTotala. */
 	static int kopuruatxtfld ;
-	  
-	
-	
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
-		
-		
+
+
 		int randomNumdado6 = (int)(Math.random() * 6 + 1);
 		int randomNumdado10 = (int)(Math.random() * 10 + 1);
 		int randomNumdado20 = (int)(Math.random() * 20 + 1);
-		
-		
+
+
 
 		frame.setLayout(null);
 		frame.add(label1); 
@@ -65,13 +92,12 @@ public class Nagusia {
 		botoia1.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) {
-			
+
 				frame.setVisible(false);
 				frame2.setLayout( new FlowLayout());
 				frame2.setVisible(true);
 				frame2.setSize(400, 800);
 				frame2.add(label1);
-
 				label1.setBounds(20,20,30,40);
 				frame2.add(botoiadado4);
 				botoiadado4.setBounds(900,425,150,50);
@@ -84,7 +110,7 @@ public class Nagusia {
 				textfield1.setSize(500, 500);
 				textfield1.setText("                                        ");
 				frame2.add(textfield1);
-				
+
 				textfield1.addActionListener(new ActionListener() { 
 					@Override
 					public void actionPerformed(ActionEvent e) {
@@ -113,24 +139,97 @@ public class Nagusia {
 
 				for (int i = 0; i < kopuruatxtfld; i++) {
 					int randomNumdado4 = (int)(Math.random() * 4 +1);
-					
+
 					dadozenbakiak = dadozenbakiak + String.valueOf(randomNumdado4);
-					 erantzuna += randomNumdado4;
-				
-					
+					erantzuna += randomNumdado4;
+
 				}
 				lbltext2.setText(dadozenbakiak);
-				lbltext3.setText(String.valueOf(erantzuna));
-				
+				lbltext3.setText("Batura : " + String.valueOf(erantzuna));
 
+			}
+		});
+		botoiadado6.addActionListener(new ActionListener() {
 
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String dadozenbakiak = "";
+				int erantzuna = 0;
+
+				frame2.setVisible(false);
+				frame3.setLayout( new FlowLayout());
+				frame3.setVisible(true);
+				frame3.add(lbltext2);
+				frame3.add(lbltext3);
+
+				//System.out.println(kopuruatxtfld);
+
+				for (int i = 0; i < kopuruatxtfld; i++) {
+					int randomNumdado6 = (int)(Math.random() * 6 +1);
+
+					dadozenbakiak = dadozenbakiak + String.valueOf(randomNumdado6);
+					erantzuna += randomNumdado6;
+
+				}
+				lbltext2.setText(dadozenbakiak);
+				lbltext3.setText("Batura : " + String.valueOf(erantzuna));
+
+			}
+		});
+		botoiadado10.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String dadozenbakiak = "";
+				int erantzuna = 0;
+
+				frame2.setVisible(false);
+				frame3.setLayout( new FlowLayout());
+				frame3.setVisible(true);
+				frame3.add(lbltext2);
+				frame3.add(lbltext3);
+
+				//System.out.println(kopuruatxtfld);
+
+				for (int i = 0; i < kopuruatxtfld; i++) {
+					int randomNumdado10 = (int)(Math.random() * 10 +1);
+
+					dadozenbakiak = dadozenbakiak + String.valueOf(randomNumdado10);
+					erantzuna += randomNumdado10;
+
+				}
+				lbltext2.setText(dadozenbakiak);
+				lbltext3.setText("Batura : " + String.valueOf(erantzuna));
 
 			}
 		});
 
+		botoiadado20.addActionListener(new ActionListener() {
 
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String dadozenbakiak = "";
+				int erantzuna = 0;
 
+				frame2.setVisible(false);
+				frame3.setLayout( new FlowLayout());
+				frame3.setVisible(true);
+				frame3.add(lbltext2);
+				frame3.add(lbltext3);
 
+				//System.out.println(kopuruatxtfld);
+
+				for (int i = 0; i < kopuruatxtfld; i++) {
+					int randomNumdado20 = (int)(Math.random() * 20 +1);
+
+					dadozenbakiak = dadozenbakiak + String.valueOf(randomNumdado20);
+					erantzuna += randomNumdado20;
+
+				}
+				lbltext2.setText(dadozenbakiak);
+				lbltext3.setText("Batura : " + String.valueOf(erantzuna));
+			}
+		});
 
 	}
 
